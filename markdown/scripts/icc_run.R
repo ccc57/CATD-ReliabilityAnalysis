557a1f9910b5af053d9e44cb46c38855574c1943
Christopher Camp <chrisclaycamp@gmail.com>
2022-03-02 22:15:20 -0500

    added subject column to psych dat

--- markdown/scripts/icc_run.R
+++ markdown/scripts/icc_run.R
@@ -6,12 +6,27 @@ run_icc <- function(icc_dat,
 
   icc_df <- data.frame()
   sub_list <- data.frame(sub_list)
+  psych_dat <- list()
+  
   measure_cols <- colnames(icc_dat %>% select(-c('subject','session')))
   pb <- txtProgressBar(min = 0, max = length(measure_cols), style = 3)
   
+  psych_files <- list.files(path = paste0(rootdir, "data/processed/connectivity_data/psych_dat_backup"), 
+                            pattern = "\\.csv$",
+                            full.names = TRUE)
+  suppressMessages(psych_dat <- lapply(psych_files, read_csv, show_col_types = FALSE))
+  
+  
+  names(psych_dat) <- gsub("\\.csv$", "", psych_files)
+  for(i in 1:length(psych_dat)) {
+    names(psych_dat[[i]])[1] <- 'subject'
+    write_csv(psych_dat[[i]], paste0(names(psych_dat[i]),'.csv'))
+  }
+  
+  psych_dat = split(psych_dat, f = psych_dat[,1])
+  
   for(x in measure_cols){
     i <- which(x == measure_cols)
-    psych_dat <- read.csv(paste0(rootdir, 'data/processed/connectivity_data/psych_dat/', substring(x,2), '.csv'))
     psych_dat <- merge(psych_dat, sub_list, by.x = 'X', by.y = 'sub_list')
     psych_dat$v1 <- apply(psych_dat %>% select(matches("v1")), 1, FUN=function(x){mean(x,na.rm = TRUE)})
     psych_dat$v4 <- apply(psych_dat %>% select(matches("v4")), 1, FUN=function(x){mean(x,na.rm = TRUE)})
@@ -22,4 +37,11 @@ run_icc <- function(icc_dat,
     icc_df <- rbind(icc_df, icc %>% filter(type == "ICC2"))
   }
   return(icc_df)
+}
+
+get_boot <- function(dat){
+  sublist <- dat$subject
+  set.seed(1)
+  bootlist <- sample(sublist, dim(sublist)[1], replace=T)
+  
 }
