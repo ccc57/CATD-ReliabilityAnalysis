4a2656687ae36513cf3e6ed4481740eea238622b
Christopher Camp <chrisclaycamp@gmail.com>
2022-03-02 23:40:48 -0500

    WIP adding bootstrap function

--- markdown/scripts/icc_run.R
+++ markdown/scripts/icc_run.R
@@ -1,47 +1,58 @@
 
 run_icc <- function(icc_dat,
-                    sub_list = unique(icc_dat&subject), 
+                    sublist = unique(icc_dat$subject), 
                     rootdir = 'C:/Users/chris/OneDrive - Yale University/Projects/ReliabilityAnalysis/',
-                    alph = 0.05){
+                    alph = 0.05,
+                    nboot = 1){
 
-  icc_df <- data.frame()
-  sub_list <- data.frame(sub_list)
+  sublist <- data.frame(sublist)
   psych_dat <- list()
   
   measure_cols <- colnames(icc_dat %>% select(-c('subject','session')))
-  pb <- txtProgressBar(min = 0, max = length(measure_cols), style = 3)
+  pb <- txtProgressBar(min = 0, max = length(measure_cols)*nboot, style = 3)
   
-  psych_files <- list.files(path = paste0(rootdir, "data/processed/connectivity_data/psych_dat_backup"), 
+  psych_files <- list.files(path = paste0(rootdir, "data/processed/connectivity_data/psych_dat"), 
                             pattern = "\\.csv$",
                             full.names = TRUE)
-  suppressMessages(psych_dat <- lapply(psych_files, read_csv, show_col_types = FALSE))
-  
-  
-  names(psych_dat) <- gsub("\\.csv$", "", psych_files)
-  for(i in 1:length(psych_dat)) {
-    names(psych_dat[[i]])[1] <- 'subject'
-    write_csv(psych_dat[[i]], paste0(names(psych_dat[i]),'.csv'))
+  if(!exists("psych_dat") || length(psych_dat) != length(psych_files)){
+    print("Reading files...")
+    suppressMessages(psych_dat <- lapply(psych_files, read_csv, show_col_types = FALSE))
+    names(psych_dat) <- gsub("\\.csv$", "", psych_files)
   }
   
-  psych_dat = split(psych_dat, f = psych_dat[,1])
+  for(i in 1:length(psych_dat)){
+    psych_dat[[i]]$v1 <- apply(psych_dat[[i]] %>% select(matches("v1")), 1, FUN=function(x){mean(x,na.rm = TRUE)})
+    psych_dat[[i]]$v4 <- apply(psych_dat[[i]] %>% select(matches("v4")), 1, FUN=function(x){mean(x,na.rm = TRUE)})
+  }
   
-  for(x in measure_cols){
-    i <- which(x == measure_cols)
-    psych_dat <- merge(psych_dat, sub_list, by.x = 'X', by.y = 'sub_list')
-    psych_dat$v1 <- apply(psych_dat %>% select(matches("v1")), 1, FUN=function(x){mean(x,na.rm = TRUE)})
-    psych_dat$v4 <- apply(psych_dat %>% select(matches("v4")), 1, FUN=function(x){mean(x,na.rm = TRUE)})
-    setTxtProgressBar(pb, i)
-    Sys.sleep(.01)
-    suppressMessages(icc <- ICC(psych_dat[,c("v1","v4")], missing=TRUE,alpha = alph/length(measure_cols))$results)
-    icc$measure <- substring(x,2)
-    icc_df <- rbind(icc_df, icc %>% filter(type == "ICC2"))
+  set.seed(1)
+  bootlist <- replicate(nboot, sample(sublist, length(sublist), replace=T))
+  bootlist[,1] <- sublist
+  
+  # for(i in 1:length(psych_dat)) {
+  #   names(psych_dat[[i]])[1] <- 'subject'
+  #   write_csv(psych_dat[[i]], paste0(names(psych_dat[i]),'.csv'))
+  # }
+  icc_df <- data.frame()
+  for(i in 1:length(measure_cols)){
+    psych_df <- data.frame()
+    for (s in 1:nboot){
+      psych_ss <- psych_dat[[i]]
+      psych_ss <- psych_ss[match(bootlist[,1],psych_ss$subject),]
+      setTxtProgressBar(pb, i*s)
+      Sys.sleep(.01)
+      suppressMessages(icc <- ICC(psych_ss[,c("v1","v4")], missing=TRUE,alpha = alph/length(measure_cols))$results)
+      icc$perm <- s
+      psych_df <-rbind(psych_df, icc %>% filter(type == "ICC2"))
+    }
+    psych_df$measure <- substring(measure_cols[i],2)
+    icc_df <- rbind(icc_df, psych_df)
   }
   return(icc_df)
 }
 
 get_boot <- function(dat){
   sublist <- dat$subject
-  set.seed(1)
-  bootlist <- sample(sublist, dim(sublist)[1], replace=T)
   
+  return(bootlist)
 }
